89ede75aa22c4f1a48a27c5af30384f75a3b4da9
sukhinlv <107252295+sukhinlv@users.noreply.github.com>
2022-06-23 18:00:28 +0300

    реализованы все методы в классе ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,29 +2,61 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    public static final int STORAGE_CAPACITY = 10_000;
+    private Resume[] storage = new Resume[STORAGE_CAPACITY];
+    private int size;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
+        if (size == STORAGE_CAPACITY) {
+            return;
+        }
+        Resume res = new Resume();
+        res.uuid = r.uuid.intern();
+        storage[size] = res;
+        size++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].toString())) {
+                Resume r = new Resume();
+                r.uuid = storage[i].uuid.intern();
+                return r;
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].toString())) {
+                int movelen = size - i - 1;
+                if (movelen > 0) {
+                    System.arraycopy(storage, i + 1, storage, i, movelen);
+                }
+                size--;
+                storage[size] = null;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] stor = new Resume[size];
+        System.arraycopy(storage, 0, stor, 0, size);
+        return stor;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
