80e1a5d1ee67049c99faecb5df401cec8556c412
sukhinlv <107252295+sukhinlv@users.noreply.github.com>
2022-07-03 22:30:58 +0300

    HW4 - исправления класса AbstractArrayStorageTest по результатам проверки ДЗ

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -8,11 +8,17 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 abstract class AbstractArrayStorageTest {
-    public static final String UUID_1 = "uuid1";
-    public static final String UUID_2 = "uuid2";
-    public static final String UUID_3 = "uuid3";
-    public static final String DUMMY = "Dummy";
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
+    private static final String DUMMY = "Dummy";
+    private static final Resume RESUME_1 = new Resume(UUID_1);
+    private static final Resume RESUME_2 = new Resume(UUID_2);
+    private static final Resume RESUME_3 = new Resume(UUID_3);
+    private static final Resume RESUME_DUMMY = new Resume(DUMMY);
     protected AbstractArrayStorage storage;
 
     public AbstractArrayStorageTest(AbstractArrayStorage storage) {
@@ -22,71 +28,87 @@ abstract class AbstractArrayStorageTest {
     @BeforeEach
     void setUp() {
         storage.clear();
-        storage.save(new Resume(UUID_1));
-        storage.save(new Resume(UUID_3));
-        storage.save(new Resume(UUID_2));
+        storage.save(RESUME_1);
+        storage.save(RESUME_3);
+        storage.save(RESUME_2);
     }
 
     @Test
     void clear() {
         storage.clear();
-        Assertions.assertAll(
-                () -> Assertions.assertEquals(storage.size(), 0),
-                () -> Assertions.assertEquals(storage.getAll().length, 0));
+        assertSize(0);
     }
 
     @Test
     void get() throws NotFoundStorageException {
         Assertions.assertAll(
-                () -> Assertions.assertEquals(storage.get(UUID_1), new Resume(UUID_1)),
-                () -> Assertions.assertThrows(NotFoundStorageException.class, () -> storage.get(DUMMY)));
+                () -> assertGet(RESUME_1),
+                () -> assertGet(RESUME_2),
+                () -> assertGet(RESUME_3),
+                () -> Assertions.assertThrows(NotFoundStorageException.class, () -> assertGet(RESUME_DUMMY)));
     }
 
     @Test
     void getAll() {
-        Assertions.assertEquals(storage.getAll().length, 3);
+        Resume[] expected = {RESUME_1, RESUME_2, RESUME_3};
+        Resume[] actually = storage.getAll();
+        Arrays.sort(actually);
+        Assertions.assertAll(
+                () -> assertSize(3),
+                () -> Assertions.assertArrayEquals(expected, actually));
     }
 
     @Test
     void size() {
-        Assertions.assertEquals(storage.size(), 3);
+        assertSize(3);
     }
 
     @Test
     void update() throws NotFoundStorageException {
+        Resume newResume = new Resume(UUID_3);
+        storage.update(newResume);
         Assertions.assertAll(
-                () -> storage.update(new Resume(UUID_2)),
-                () -> Assertions.assertEquals(storage.size(), 3),
+                () -> assertSize(3),
+                () -> Assertions.assertSame(newResume, storage.get(newResume.getUuid())),
                 () -> Assertions.assertThrows(NotFoundStorageException.class, () -> storage.update(new Resume(DUMMY))));
     }
 
     @Test
     void delete() throws NotFoundStorageException {
+        storage.delete(UUID_2);
         Assertions.assertAll(
-                () -> storage.delete(UUID_2),
-                () -> Assertions.assertThrows(NotFoundStorageException.class, () -> storage.delete(DUMMY)),
-                () -> Assertions.assertEquals(storage.size(), 2));
+                () -> assertSize(2),
+                () -> Assertions.assertThrows(NotFoundStorageException.class, () -> storage.delete(DUMMY)));
     }
 
     @Test
     void save() throws FoundStorageException {
+        Resume newResume = new Resume(DUMMY);
+        storage.save(newResume);
         Assertions.assertAll(
-                () -> Assertions.assertThrows(FoundStorageException.class, () -> storage.save(new Resume(UUID_1))),
-                () -> {
-                    storage.save(new Resume(DUMMY));
-                    Assertions.assertEquals(storage.size(), 4);
-                });
+                () -> assertSize(4),
+                () -> assertGet(newResume),
+                () -> Assertions.assertThrows(FoundStorageException.class, () -> storage.save(new Resume(UUID_1))));
     }
 
     @Test
     void overflow() throws StorageException {
+        storage.clear();
         try {
-            for (int i = storage.size(); i < AbstractArrayStorage.STORAGE_CAPACITY; i++) {
+            for (int i = 0; i < AbstractArrayStorage.STORAGE_CAPACITY; i++) {
                 storage.save(new Resume());
             }
         } catch (StorageException e) {
             Assertions.fail("Unexpected storage overflow", e);
         }
         Assertions.assertThrows(StorageException.class, () -> storage.save(new Resume(DUMMY)));
+    }
+
+    void assertGet(Resume resume) {
+        Assertions.assertEquals(resume, storage.get(resume.getUuid()));
+    }
+
+    void assertSize(int size) {
+        Assertions.assertEquals(size, storage.size);
     }
 }
